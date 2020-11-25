9009c37e51f610e8f80a89f81df0e1658a2104f2
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 03:37:15 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,7 +5,7 @@ def my_collect(collection)
     i = 0
     new_collection = []
     while i < collection.length
-      yield new_collection << collection[i]
+      new_collection << yield(collection[i])
       i += 1
     end
     new_collection.
